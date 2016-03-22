9ae8ffb45409771fd5aadd5d7333aa0d5e4f07a2
Coline Forde <coline.forde@gmail.com>
2016-03-22 12:25:12 -0700

    add admin page and controller

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,9 +1,9 @@
 class Product < ActiveRecord::Base
 	validates :name, :description, :price, :quantity, presence: true
-  belongs_to :user
+  	belongs_to :user
 
-  validates :name, :description, :price, :quantity, presence: true
+  	validates :name, :description, :price, :quantity, presence: true
 
-  scope :sort_by_price, -> { order(:price) }
+  	scope :sort_by_price, -> { order(:price) }
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   resources :users
   resources :products
+  resources :admin
   
   root 'index#index'
+
 end
