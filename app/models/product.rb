997cdb14e69de5560ed1d990342ba7aa068b4374
Coline Forde <coline.forde@gmail.com>
2016-03-21 21:20:07 -0700

    add rspecs for controller and models

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,4 @@
 class Product < ActiveRecord::Base
+	validates :name, :description, :price, :quantity, presence: true
   belongs_to :user
 end
---
+++ spec/controllers/product_controller_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+RSpec.describe ProductsController, type: :controller do
+	describe 'GET #new' do 
+		it 'assigns a new product to @product' do 
+			get :new
+			expect(assigns(:product)).to be_a_new(Product)
+		end
+
+		it 'renders the :new template' do 
+			get :new
+			expect(response).to render_template :new
+		end
+	end
+	describe 'GET #edit' do 
+		it 'assigns the requested product to @product' do 
+			product = create(:product)
+			get :edit, id: product
+			expect(assigns(:contact)).to eq contact
+		end
+
+		it 'renders the :edit template' do 
+			product = create(:product)
+			get :edit, id: product
+			expect(response).to render_template :edit
+		end
+	end
+end
+
