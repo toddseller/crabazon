0db27872214b7c24c860d9a543ec8edc1e29c5a6
Todd Seller <me@toddseller.com>
2016-03-24 10:25:56 -0700

    Continue work on categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -20,7 +20,7 @@ class CategoriesController < ApplicationController
 
   def show
     @category = Category.find(params[:id])
-    @products = @category.products
+    @products = @category.products.sort_by_price
   end
 
   def edit
