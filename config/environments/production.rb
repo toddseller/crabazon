007371aebebdd0ff6427e5eb3b3f6b59e69ac35b
Todd Seller <me@toddseller.com>
2016-03-24 11:12:06 -0700

    Test password

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,8 +67,8 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
     port:                 587,
-    user_name:            ENV['gmail_username'],
-    password:             ENV['gmail_password'],
+    user_name:            'oldfatbastard666@gmail.com',
+    password:             'babyfacesbitch',
     authentication:       'plain',
     enable_starttls_auto: true
   }
