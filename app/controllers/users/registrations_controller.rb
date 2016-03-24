97f066b252e39722591559a311fcfbd23a541fa6
Todd Seller <me@toddseller.com>
2016-03-24 08:49:15 -0700

    Add product category drop down to nav and other improvements

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -49,7 +49,7 @@ before_filter :configure_sign_up_params, only: [:create]
   #   super
   # end
 
-  # protected
+  protected
 
   # If you have extra params to permit, append them to the sanitizer.
   def configure_sign_up_params
@@ -62,12 +62,12 @@ before_filter :configure_sign_up_params, only: [:create]
   # end
 
   # The path used after sign up.
-  # def after_sign_up_path_for(resource)
-  #   super(resource)
-  # end
+  def after_sign_up_path_for(resource)
+    '/index'
+  end
 
   # The path used after sign up for inactive accounts.
-  # def after_inactive_sign_up_path_for(resource)
-  #   super(resource)
-  # end
+  def after_inactive_sign_up_path_for(resource)
+    '/index'
+  end
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,5 @@
 class Category < ActiveRecord::Base
   has_and_belongs_to_many :products
+
+  scope :sort_by_name, -> { order(:name) }
 end
