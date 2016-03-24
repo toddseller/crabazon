91a2f00b7a061a6a075ca6f3edb4993701ae566c
Brigitte Kozena & Todd Seller <pair+brigittekozena.toddseller@devbootcamp.com>
2016-03-23 22:09:57 -0700

    Add role column to Users table

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,32 +1,15 @@
 class UsersController < ApplicationController
-  # before_action :authenticate_user!
+  before_action :authenticate_user!
 
   def new
     @user = User.new
   end
 
   def create
-    p '*********************'
-    p params
-    @user = User.new(params[:email])
-    p '*********************'
-    p @user
-    respond_to do |format|
-      if @user.save
-        p '****************'
-        p 'Bitches'
-        UserMailer.welcome_email(@user).deliver_now
-        format.html { redirect_to(@user, notice: 'User was successfully created.') }
-        format.json { render json: @user, status: :created, location: @user }
-      else
-        format.html { render action: 'new' }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
-    end
+
   end
 
   def show
-    @user = User.find(1)
     @products = Product.sort_by_price
     render 'index/index'
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable, password_length: 6..72
 
   has_many :products, foreign_key: "reseller_id"
 end
