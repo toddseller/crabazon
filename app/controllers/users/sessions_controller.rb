56e830de9fbfcb2c0faf1c2ef920257720651de5
Brigitte Kozena & Todd Seller <pair+brigittekozena.toddseller@devbootcamp.com>
2016-03-23 23:03:50 -0700

    Configure login redirects for admin and user

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -2,62 +2,32 @@ class Users::SessionsController < Devise::SessionsController
 # before_filter :configure_sign_in_params, only: [:create]
 
   # GET /resource/sign_in
-  # def new
-  #   super
-  # end
+  def new
+    super
+  end
 
   # POST /resource/sign_in
-  # def create
-  #   super
-
-  #   p '*********************'
-  #   p params
-  #   @user = User.new(params[:email])
-  #   p '*********************'
-  #   p @user
-  #   respond_to do |format|
-  #     if @user.save
-  #       p '****************'
-  #       p 'Bitches'
-  #       UserMailer.welcome_email(@user).deliver_now
-  #       format.html { redirect_to(@user, notice: 'User was successfully created.') }
-  #       format.json { render json: @user, status: :created, location: @user }
-  #     else
-  #       format.html { render action: 'new' }
-  #       format.json { render json: @user.errors, status: :unprocessable_entity }
-  #     end
-  #   end
-  # end
-
-  # def create
-  #   p '*********************'
-  #   p params
-  #   @user = User.new(configure_sign_in_params)
-  #   p '*********************'
-  #   p @user
-  #   respond_to do |format|
-  #     if @user.save
-  #       p '****************'
-  #       p 'Bitches'
-  #       UserMailer.welcome_email(@user).deliver_now
-  #       format.html { redirect_to(@user, notice: 'User was successfully created.') }
-  #       format.json { render json: @user, status: :created, location: @user }
-  #     else
-  #       format.html { render action: 'new' }
-  #       format.json { render json: @user.errors, status: :unprocessable_entity }
-  #     end
-  #   end
-  # end
+  def create
+    super
+  end
 
   # DELETE /resource/sign_out
   # def destroy
   #   super
   # end
 
-  # protected
+  protected
 
   # If you have extra params to permit, append them to the sanitizer.
   def configure_sign_in_params
-    devise_parameter_sanitizer.for(:sign_in) << :attribute
+    devise_parameter_sanitizer.for(:sign_in, :role) << :attribute
+  end
+
+  def after_sign_in_path_for(resource)
+    if current_user.role == 'admin'
+      admin_index_path
+    else
+      index_path
+    end
   end
 end
