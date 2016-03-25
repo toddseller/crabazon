22a0c374128be10598839d337f2878ba433f3838
Brigitte Kozena <brigittekozena@gmail.com>
2016-03-25 15:37:55 -0700

    Database adding Orders, OrdersProducts associations

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160325212605) do
+ActiveRecord::Schema.define(version: 20160325221604) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,21 +33,22 @@ ActiveRecord::Schema.define(version: 20160325212605) do
   add_index "categories_products", ["product_id"], name: "index_categories_products_on_product_id", using: :btree
 
   create_table "orders", force: :cascade do |t|
-    t.integer "user_id"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  add_index "orders", ["user_id"], name: "index_orders_on_user_id", using: :btree
-
   create_table "orders_products", force: :cascade do |t|
-    t.integer "product_id"
-    t.integer "order_id"
+    t.integer  "product_id"
+    t.integer  "order_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   add_index "orders_products", ["order_id"], name: "index_orders_products_on_order_id", using: :btree
   add_index "orders_products", ["product_id"], name: "index_orders_products_on_product_id", using: :btree
 
   create_table "products", force: :cascade do |t|
-    t.integer  "reseller_id"
     t.string   "name"
     t.decimal  "price",         precision: 8, scale: 2
     t.string   "description"
