db924eea632ea90a35a8ad958e651fa2a12434a3
Todd Seller <me@toddseller.com>
2016-03-25 14:47:09 -0700

    Add multiselect dropdown with checkbox logic

--- app/assets/javascripts/admin.js
+++ app/assets/javascripts/admin.js
@@ -2,4 +2,23 @@ $(document).ready(function() {
   $('.clickable-row').on('click', function() {
     window.document.location = $(this).data('url');
   });
+
+  $(document).bind('click', function(event) {
+    var $clicked = $(event.target);
+  });
+
+  $('.multiSelect input[type="checkbox"]').on('click', function() {
+    var title = $(this).closest('.multiSelect,li').text()+',';
+
+    if ($(this).is(':checked')) {
+      var html = '<span title="' + title + '">' + title + '</span>';
+      $('.multiSel').append(html);
+      $('.multiSel').css('text-align', 'left');
+      $('.hida').hide();
+    } else {
+      $('span[title="' + title + '"]').remove();
+      var ret = $('.hida');
+      $('.dropdown button span').append(ret);
+    }
+  });
 });
--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,7 +1,7 @@
 class CategoriesController < ApplicationController
 
   def index
-    @categories = Category.all
+    @categories = Category.sort_by_name
   end
 
   def new
