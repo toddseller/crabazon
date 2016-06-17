d500c9aa14b6f90323434047e9da8e4b60088444
Todd Seller <me@toddseller.com>
2016-06-17 10:52:22 -0700

    Update production smtp settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,6 +67,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
     port:                 587,
+    domain:               "crabazon.herokuapp.com",
     user_name:            ENV['EMAIL_ADDRESS'],
     password:             ENV['EMAIL_SECRET'],
     authentication:       :login,
